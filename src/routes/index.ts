0cd89b0021569ed422b05fbe87aad3a2918286ba
Alan Almeida <alandavidalmeida@gmail.com>
2023-07-26 18:13:12 -0300

    reconfiguration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@
 
 import express, {Request, Response } from 'express';
 import helloRouter from './HelloRouter';
-import { LogInfo } from '@/utils/logger';
+import { LogInfo } from '../utils/logger';
 
 //Server instance
 let server = express();
@@ -15,7 +15,16 @@ let rootRouter = express.Router();
 
 // Activate for requests to hhtp//localhost:8000/api
 
+// GET: htttp://localhost:8000/api/
 rootRouter.get('/', (req: Request, res: Response) => {
+    LogInfo('GET: http://localhost:8000/api/')
     //Send Hello World
     res.send('Welcome to my API restful: Express + TS + Nodemon + Jest + Swagger + Mongoose');
 });
+
+// Redirections to Routers & controllers
+server.use('/', rootRouter); //htttp://localhost:8000/api/
+server.use('/hello', helloRouter) //htttp://localhost:8000/api/hello
+// Add more routes to the app
+
+export default server;
---
+++ src/server/index.ts
@@ -0,0 +1,41 @@
+import express, { Express, Request, Response } from "express";
+
+// Security
+import cors from 'cors';
+import helmet from 'helmet';
+
+// TODO HTTPS
+
+// Root Router
+
+import rootRuter from '../routes';
+
+
+//Create express app
+const server: Express = express();
+
+
+// Define server to use "/api" and use RootRouter
+// http://localhost:8000/api/...
+server.use(
+    '/api',
+    rootRuter
+);
+
+//TODO moongose conecction
+
+//Security config
+server.use(helmet());
+server.use(cors());
+
+// Content type config
+server.use(express.urlencoded({extended: true, limit: '50mb'}));
+server.use(express.json({limit: '50mb'}));
+
+// http://localhost:8000/ --> http://localhost:8000/api/
+server.get('/', (req: Request, res: Response) => {
+    res.redirect('/api');
+});
+
+export default server;
+
